1624c23acd58faed1c5708dbc0ecbdf481bfc298
alexsemagin <alex-semagin_09@mail.ru>
2017-04-14 13:03:01 +0300

    Finish in MyNewBranch

--- app/src/main/java/com/example/robot/myapp2/ui/test_task/MapFragment.java
+++ app/src/main/java/com/example/robot/myapp2/ui/test_task/MapFragment.java
@@ -172,8 +172,8 @@ public class MapFragment extends Fragment implements OnMapLongClickListener, OnM
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        }
-        locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
+        } else
+            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
     }
 
     @Override
